544db7586af6b4bb86dae13179fa8a8c76a83ae4
Vincent Le <vincentle@MacBookAir.fios-router.home>
2026-03-17 21:46:14 -0700

    authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,11 +4,10 @@ const bcrypt = require('bcryptjs');
 const userSchema = mongoose.Schema(
   {
     name: { type: 'String', required: true },
-    email: { type: 'String', unique: true, required: true },
+    email: { type: 'String', unique: true, required: true, unique: true },
     password: { type: 'String', required: true },
     pic: {
       type: 'String',
-      required: true,
       default:
         'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg',
     },
