3965a7886dd8b9919f40bdd6ab379b00a9ff8a45
Vincent Le <vincentle@MacBookAir.fios-router.home>
2026-03-22 00:09:55 -0700

    fix express 5 wildcard route

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname1, '/frontend/build')));
-  app.get('*', (req, res) =>
+  app.get('*splat', (req, res) =>
     res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'))
   );
 } else {
